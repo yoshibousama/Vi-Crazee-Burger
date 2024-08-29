a2b4d4fad856b88a7f67776261d060f8beff14e7
yoshibousama <aomine-sama@outlook.fr>
2024-08-29 07:30:53 +0200

    App.jsx > Add pseudo

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Andy <code>src/App.jsx</code> and save to test HMR
+          Yoshibou<code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
